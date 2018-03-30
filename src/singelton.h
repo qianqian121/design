7e75cbc325e952afe4aed77e48b874bfec39026d
qianqian121 <qianqian141@hotmail.com>
2018-03-30 11:43:04 -0700

    Update singelton.h

--- src/singelton.h
+++ src/singelton.h
@@ -10,3 +10,23 @@ private:
     Singleton();
     ~Singleton();
 };
+
+
+
+
+template<typename HeirT>
+class Singleton
+{
+public:
+    Singleton() = delete;
+
+    Singleton(const Singleton &) = delete;
+
+    Singleton &operator=(const Singleton &) = delete;
+
+    static HeirT &instance()
+    {
+        static HeirT instance;
+        return instance;
+    }
+};
